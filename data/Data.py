e1ab791b2b233165be9a111c90d644b259a39231
wengcan <572037922@qq.com>
2023-04-20 13:59:33 +0800

    feat: add cost of a task

--- data/Data.py
+++ data/Data.py
@@ -4,7 +4,7 @@ import asyncio
 from urllib.parse import urlparse
 import mysql.connector
 from mysql.connector import errorcode
-from .values import TaskStatus,OutputType
+from .values import TaskStatus,OutputType,Cost
 from .FileHandler import FileHandler
 
 
@@ -48,14 +48,38 @@ class Data():
         cursor.close()
     def __insert_task(self, params: dict):
         cursor = self.cnx.cursor()
-        add_task = (
-            "INSERT INTO `tasks` "
-            "(`taskID`,`type`,`parent`,`v_index`,`u_index`,`status`,`message_id`,`image_hash`,`url_global`,`url_cn`,`create_at`) "
-            "VALUES ( %(taskId)s, %(type)s, %(parent)s, %(v_index)s, %(u_index)s, %(status)s, %(message_id)s, %(image_hash)s, %(url_global)s, %(url_cn)s, NOW())"
-        )
-        cursor.execute(add_task, params)
-        self.cnx.commit()
-        cursor.close()           
+        try:
+            self.cnx.start_transaction()
+            #### get token_id ###
+            sql = "SELECT `token_id` FROM `prompts` WHERE taskId = %s"
+            val = (params['taskId'],)
+            cursor.execute(sql, val)
+            record = cursor.fetchone()
+            if record is not None:
+                token_id = record[0]
+                add_task = (
+                    "INSERT INTO `tasks` "
+                    "(`taskID`,`type`,`parent`,`v_index`,`u_index`,`status`,`message_id`,`image_hash`,`url_global`,`url_cn`) "
+                    "VALUES ( %(taskId)s, %(type)s, %(parent)s, %(v_index)s, %(u_index)s, %(status)s, %(message_id)s, %(image_hash)s, %(url_global)s, %(url_cn)s)"
+                )
+                cursor.execute(add_task, params)
+                insertd_task_id = cursor.lastrowid
+                ##### add the cost ####
+                if params['status'] == TaskStatus.FINISHED.value:
+                    cost_sql = ("INSERT INTO `token_audit` (`token_id`, `task_id`, `cost`) VALUES (%(token_id)s, %(task_id)s,%(cost)s)")
+                    cost_row = {
+                        'token_id' : token_id,
+                        'task_id' : insertd_task_id,
+                        'cost': Cost.get_cost(params['type'])
+                    }
+                    cursor.execute(cost_sql, cost_row)
+            self.cnx.commit()
+
+        except Exception as e:
+            print(e)
+            self.cnx.rollback()
+        finally:
+            cursor.close()         
     def add_task(self, tokenId,  taskId, prompt):
         self.r.setex(taskId, 10 * 60 , prompt )
         self.__insert_prompt({
--- data/values.py
+++ data/values.py
@@ -10,3 +10,18 @@ class OutputType(Enum):
     DRAFT = 1
     VARIATION = 2
     UPSCALE = 3
+
+
+class Cost(Enum):
+    DRAFT = 4
+    VARIATION = 4
+    UPSCALE = 2
+    @staticmethod
+    def get_cost(output: OutputType):
+        if output == OutputType.DRAFT.value:
+            return Cost.DRAFT.value
+        if output == OutputType.VARIATION.value:
+            return Cost.VARIATION.value
+        if output == OutputType.UPSCALE.value:
+            return Cost.UPSCALE.value  
+        return 0
